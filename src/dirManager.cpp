f072649ca66a863c7260a00afe919fdfcc965eed
disintegrate <oleajascha@gmail.com>
2025-07-06 19:43:41 +0800

    added remove directory function.
    the opposite of adding.

--- src/dirManager.cpp
+++ src/dirManager.cpp
@@ -14,7 +14,7 @@ void Config::createConfig(const string &fileName) {
     std::vector<string> dirLabels = {"prefered documents", "downloads", "desktop"};
     std::vector<fs::path> paths(3);
     
-    cout << "If you already have your existing documents folder is decently sorted.\n"
+    cout << "If you already have your existing documents folder is decently sorted,\n"
          << "I suggest you to create a new secondary documents folder.\n\n";
 
     for(size_t i = 0; i < dirLabels.size(); ++i) {
@@ -30,15 +30,15 @@ void Config::createConfig(const string &fileName) {
     }
     hUtils::log.Success("\nDirectory Accepted:");
     for(const auto &path : paths) {
-        cout << '\t' << path << '\n';
+        cout << '\t' << path.string() << '\n';
     }
     hUtils::sleep(2000);
 
     std::ofstream file(fileName);
 
     if(file.is_open()) {
-        file << "sourceDirectories=" << paths[0] << '\n';
-        file << "initialDirectories=" << paths[1] << "," << paths[2];
+        file << "sourceDirectories=" << paths[0].string() << '\n';
+        file << "initialDirectories=" << paths[1].string() << "," << paths[2].string();
         hUtils::log.Success("File created: " + fileName);
     }
     else {
@@ -64,8 +64,10 @@ void Config::addDirectory(const string &fileName) {
     cout << "\nWhere do you want to add this directory?\n"
          << "1. Source Directories\n"
          << "2. Initial Directories\n"
-         << "3. Cancel\n";
+         << "3. Cancel\n"
+         << hUtils::text.fgColor(93) << "Choose action: " << hUtils::text.defaultText();
          std::cin >> choice;
+    hUtils::text.toLine();
 
     switch(choice) {
         case 1:
@@ -87,19 +89,98 @@ void Config::addDirectory(const string &fileName) {
     if(file.is_open()) {
         file << "sourceDirectories=";
         for (size_t i = 0; i < srcDir.size(); ++i) {
-            file << srcDir[i];
+            file << srcDir[i].string();
             if(i != srcDir.size() -1) file << ",";
         }
         file << '\n';
 
         file << "initialDirectories=";
         for(size_t i = 0; i < initDir.size(); ++i) {
-            file << initDir[i];
+            file << initDir[i].string();
             if(i != initDir.size() - 1) file << ",";
         }
 
         file.close();
         hUtils::log.Success("Cofiguarion file updated successfully");
+    } else {
+        hUtils::log.Error("Could not update config file");
+    }
+}
+
+void Config::removeDirectory(const string &fileName) {
+    int choice = 0;
+    string input;
+    fs::path pathToRemove;
+
+    cout << "\nWhere do you want to remove a directory from?\n"
+         << "1. Source Directories\n"
+         << "2. Initial Directories\n"
+         << "3. Cancel\n"
+         << hUtils::text.fgColor(93) << "Choose action: " << hUtils::text.defaultText();
+    std::cin >> choice;
+
+    std::vector<fs::path>* targetDir = nullptr;
+    std::string dirType;
+
+    switch (choice) {
+        case 1:
+            targetDir = &srcDir;
+            dirType = "source";
+            break;
+        case 2:
+            targetDir = &initDir;
+            dirType = "initial";
+            break;
+        case 3:
+            cout << "Operation canceled\n";
+            return;
+        default:
+            cout << "Invalid choice. Please enter 1, 2, or 3\n";
+            return;
+    }
+
+    if (targetDir->max_size() <= 1) {
+        hUtils::log.Warning("You only have 1 assigned " + dirType + " directory!");
+        hUtils::sleep(4000);
+        return;
+    }
+
+    cout << "\nSelect the directory to remove:\n";
+    for (size_t i = 0; i < targetDir->size(); ++i) {
+        cout << i + 1 << ". " << (*targetDir)[i].string() << '\n';
+    }
+
+    size_t removeIndex;
+    cout << "\nEnter the number of the directory to remove: ";
+    std::cin >> removeIndex;
+    hUtils::text.toLine();
+
+    if (removeIndex < 1 || removeIndex > targetDir->size()) {
+        hUtils::log.Error("Invalid index.");
+        return;
+    }
+
+    fs::path removed = (*targetDir)[removeIndex - 1];
+    targetDir->erase(targetDir->begin() + (removeIndex - 1));
+    hUtils::log.Success("Removed: " + removed.string());
+
+    std::ofstream file(fileName);
+    if(file.is_open()) {
+        file << "sourceDirectories=";
+        for (size_t i = 0; i < srcDir.size(); ++i) {
+            file << srcDir[i].string();
+            if(i != srcDir.size() -1) file << ",";
+        }
+        file << '\n';
+
+        file << "initialDirectories=";
+        for(size_t i = 0; i < initDir.size(); ++i) {
+            file << initDir[i].string();
+            if(i != initDir.size() - 1) file << ",";
+        }
+
+        file.close();
+        hUtils::log.Success("Configuration file updated successfully");
     } else {
         hUtils::log.Error("Could not update config file");
     }
