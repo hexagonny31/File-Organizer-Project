d7cdec5c3fac1cf6d2e64dd247c4fa6bfcd42c47
disintegrate <oleajascha@gmail.com>
2026-03-12 10:37:35 +0800

    Isolated roots & added unused tuple variable.

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ class MainWindow(QWidget):
         super().__init__()
         
         self.config_file = "settings.json"
-        self.source_dir = []
+        self.roots = [] # search roots list. where the program will look for files to move.
+        self.targets = [] # target directories list. where the files will be sorted into.
+        self.tuples = [  # list of (folder_name, extensions) touples. the program will sort files into the folder_name in the targetted folder.
+            ("folder_name", [".pdf","docx"])
+        ]
+        
+        self.runtime_map = {}
         
         self.loadData()
         self.initUI()
@@ -20,32 +26,55 @@ class MainWindow(QWidget):
         if file_path.exists():
             with open(file_path, 'r') as f:
                 data = json.load(f)
-                self.source_dir = [Path(p) for p in data]
+            
+            self.roots = [Path(p) for p in data["search_roots"]]
+            self.tuples = data["sorting_rules"]
+        else:
+            self.saveData() # create the file if it doesn't exist.
+            
+        self.runtime_map = { # flattening tuples for backend operations.
+            ext: folder 
+            for folder, ext_list in self.tuples 
+            for ext in ext_list
+        }
     
     def saveData(self):
-        str_to_save = [str(p) for p in self.source_dir]
+        str_to_save = [str(p) for p in self.roots]
         with open(self.config_file, 'w') as f:
             json.dump(str_to_save, f, indent=4)
         
     def initUI(self):
         self.setWindowTitle("File Organizer Project")
         self.setWindowIcon(QIcon("lain.ico"))
-        self.resize(550, 400) # width, height
+        self.setFixedSize(550, 400) # width, height
         
         main_layout = QVBoxLayout()
         
-        src_layout_add = QHBoxLayout()
-        self.src_path_input = QLineEdit()
-        self.src_path_input.setPlaceholderText("Select a folder...")
+        main_layout.addLayout(self.rootLayout()) # where you add/remove search roots.
+        self.setLayout(main_layout)
+        
+        self.browse_button.setDefault(True)
+        self.browse_button.clicked.connect(self.browseFolder)
+        self.add_button.setDefault(True)
+        self.add_button.clicked.connect(self.addSourceDirectory)
+        self.remove_button.setDefault(True)
+        self.remove_button.clicked.connect(self.removeSourceDirectory)
+        for path_obj in self.roots:
+            self.combo_box.addItem(str(path_obj))
+
+    def rootLayout(self):
+        root_layout_add = QHBoxLayout() # source directory add layout
+        self.root_path_input = QLineEdit()
+        self.root_path_input.setPlaceholderText("Select a folder...")
         
         self.browse_button = QPushButton("&Browse...")
         self.add_button = QPushButton("Add")
         
-        src_layout_add.addWidget(self.src_path_input)
-        src_layout_add.addWidget(self.browse_button)
-        src_layout_add.addWidget(self.add_button)
+        root_layout_add.addWidget(self.root_path_input)
+        root_layout_add.addWidget(self.browse_button)
+        root_layout_add.addWidget(self.add_button)
         
-        src_layout_remove = QHBoxLayout()
+        root_layout_remove = QHBoxLayout() # source directory remove layout
         self.combo_box = QComboBox()
         self.remove_button = QPushButton("Remove Selected")
         self.remove_button.setSizePolicy(
@@ -62,57 +91,44 @@ class MainWindow(QWidget):
             }
         """)
         
-        src_layout_remove.addWidget(self.combo_box)
-        src_layout_remove.addWidget(self.remove_button)
+        root_layout_remove.addWidget(self.combo_box)
+        root_layout_remove.addWidget(self.remove_button)
 
-        src_layout_container = QVBoxLayout()
+        root_layout_container = QVBoxLayout()
 
-        src_layout_container.addLayout(src_layout_add)
-        src_layout_container.addLayout(src_layout_remove)
-        main_layout.addWidget(QLabel("Manage Source Directories:"))
-        main_layout.addLayout(src_layout_container)
-        
-        self.setLayout(main_layout)
-        
-        self.browse_button.setDefault(True)
-        self.browse_button.clicked.connect(self.browseFolder)
-        self.add_button.setDefault(True)
-        self.add_button.clicked.connect(self.addSourceDirectory)
-        self.remove_button.setDefault(True)
-        self.remove_button.clicked.connect(self.removeSourceDirectory)
-        for path_obj in self.source_dir:
-            self.combo_box.addItem(str(path_obj))
-
+        root_layout_container.addWidget(QLabel("Manage Search Roots:"))
+        root_layout_container.addLayout(root_layout_add)
+        root_layout_container.addLayout(root_layout_remove)
+        return root_layout_container
+    
     def browseFolder(self):
         folder_selected = QFileDialog.getExistingDirectory(self, "Select Directory")
         if folder_selected:
-            self.src_path_input.setText(folder_selected)
+            self.root_path_input.setText(folder_selected)
             
     def addSourceDirectory(self):
-        raw_str = self.src_path_input.text().strip()
-        
+        raw_str = self.root_path_input.text().strip()
         if not raw_str:
             return
         
         path_obj = Path(raw_str)
-        
         if not path_obj.is_dir():
             QMessageBox.warning(self, "Invalid Path", "This folder doesn't exist!")
             return     
-        if path_obj in self.source_dir:
+        if path_obj in self.roots:
             QMessageBox.warning(self, "Duplicate Entry", f"The path is already in the list.")
-            self.src_path_input.clear()
+            self.root_path_input.clear()
             return
         
-        self.source_dir.append(path_obj)
+        self.roots.append(path_obj)
         self.combo_box.addItem(str(path_obj))
         self.saveData()
-        self.src_path_input.clear()
+        self.root_path_input.clear()
     
     def removeSourceDirectory(self):
         index = self.combo_box.currentIndex()
         if index != -1:
-            self.source_dir.pop(index)
+            self.roots.pop(index)
             self.combo_box.removeItem(index)
             self.saveData()
 
